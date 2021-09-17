fff39f6db4bafd688b8b7d6f7c1fbaa67b4f7407
ArisGoi <aris.goi97@gmail.com>
2021-09-17 17:09:05 +0200

    add vue

---
+++ main.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+    el: "#root",
+    data:{
+        images: [
+            "imgs/cibo.jpg",
+            "imgs/insegna.jpg",
+            "imgs/mare.jpg",
+            "imgs/mare2.jpg",
+            "imgs/scrivania.jpg"
+        ],
+        imgIndex: 0,
+    },
+    methods:{
+        prevImg: function(){
+            this.imgIndex--;
+            if (this.imgIndex < 0) {
+                this.imgIndex = this.images.lenght;
+            }
+        },
+
+        nextImg: function(){
+            this.imgIndex++;
+            if (this.imgIndex >= app.images.lenght - 1) {
+                app.imgIndex = 0;
+            }
+        },
+    },
+});
